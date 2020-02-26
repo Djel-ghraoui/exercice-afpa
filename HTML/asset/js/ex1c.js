c97a9b42a2a2135cdb3c9292221998d46cd0bcbb
Fodé <mauro.fadika@afpa.fr>
2020-02-26 16:55:34 +0100

    correction exemple 1 et 2

--- HTML/asset/js/ex1c.js
+++ HTML/asset/js/ex1c.js
@@ -1,36 +1,53 @@
 // Exemple 1...
-if("Température > 38");{
-("Le patient a de la fièvre");
+if ("Température > 38"); {
+    ("Le patient a de la fièvre");
+}
+// Correction
+var Temperature = 0;
+if (Temperature > 38) {
+    console.log("Le patient a de la fièvre");
 }
 
 
 // Exemple 2...
-if("Température > 41") && ("Tension > 25");{
-    ("Le patient va perdre patience");
+// if ("Température > 41") && ("Tension > 25"); {
+//     ("Le patient va perdre patience");
+// }
+
+// Correction
+var Temperature = 51;
+var Tension = 26;
+if (Temperature > 41 && Tension > 25) {
+    console.log("Le patient va perdre patience");
 }
 
 
 
 // Exemple 3...
-if("non Patient");{
+if ("non Patient"); {
     ("Econduire l'olibrius");
 }
 
-
-
-// Exemple 4...
-if("Température > 42") II ("Tension >= 25") && ("Pouls > 180"){
-    ("Prévenir la famille")
-}
-A
-
-// Exemple 5...
-if("Température > 40") II ("Tension >= 25"){
-    ("Hospitaliser le patient")
+// Exemple 3...
+if (non patient) {
+    console.log("Econduire l 'olibrius")
 }
 
 
-// Exemple 6...
-if("Patient") && ("Pouls = 0"){
-    ("Appeler le curé")
-}
+
+// // Exemple 4...
+// if ("Température > 42") II("Tension >= 25") && ("Pouls > 180") {
+//     ("Prévenir la famille")
+// }
+// A
+
+// // Exemple 5...
+// if ("Température > 40") II("Tension >= 25") {
+//     ("Hospitaliser le patient")
+// }
+
+
+// // Exemple 6...
+// if ("Patient") && ("Pouls = 0") {
+//     ("Appeler le curé")
+// }
